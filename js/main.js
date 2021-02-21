8523a922836b7e0c03568911794bb011ba6d07a7
Enes Malik Özer <enesmozer@gmail.com>
2021-02-21 21:24:16 +0300

    animation and last design

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $('.button').on('click', function () {
+      $(".focus").css({ 'top': '47%', 'left': '41.5%' });
       $('.button').removeClass('active');
       $( document.activeElement).addClass('active');
 }) 
